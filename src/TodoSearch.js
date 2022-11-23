7943466c48962c4d486f273777868d1a8d1f326e
Leandro Silva <leeansilva@gmail.com>
2022-11-23 17:39:40 -0300

    manejo de estados con useState

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import './TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onChangeSearchValue = (e)=>{
         console.log(e.target.value);
+        setSearchValue(e.target.value)
+
     }
     return (
+        <Fragment>
         <div className="TodoDiv">
             <input 
-            placeholder='Cebolla' 
+            placeholder='Cebolla'
+            value={searchValue} 
             onChange={onChangeSearchValue}
             />
         </div>
+        <p>{searchValue}</p>
+        </Fragment>
     )
 }
 
